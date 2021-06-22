fb32fb518698fce790965effc7bca72843f5541f
purisquiri <mau.dorigo@outlook.es>
2021-06-22 16:53:31 -0400

    passing props to parent with slot

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <!-- <hello-world v-slot="v"> -->
+    <hello-world #default="{ user, favorites }">
+      <p>Hello {{ user.name }} you like {{ favorites[0] }}</p>
+    </hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
